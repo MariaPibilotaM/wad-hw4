2916ef18259b7433dae3fbf387d05d3ebfad8eb1
vidriktt <vidriktoom@gmail.com>
2020-01-02 15:13:41 +0200

    task 4

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,13 +1,25 @@
 import { mount } from '@vue/test-utils'
 import List from "../../src/components/List";
+import Item from '../../src/models/Item.js'
 
 
 describe('List', () => {
     const wrapper = mount(List)
 
-    const vm = wrapper.vm
-
     it('Check if no item is provided -> displays text',()=>{
         expect(wrapper.html()).toContain('Add your first Todo task')
+    })
+    
+    it('When an item is marked as done -> item is updated correctly', async () => {
+        let empty = []
+        expect(wrapper.props().list).toEqual(empty)
+
+        let TestItem = new Item("NewItem")
+        wrapper.props().list.push(TestItem)
+        await wrapper.vm.$nextTick()
+        expect(wrapper.props().list[0].done).toBe(false)
+        wrapper.find('span').trigger('click')
+        await wrapper.vm.$nextTick()
+        expect(wrapper.props().list[0].done).toBe(true)
     })
 });
